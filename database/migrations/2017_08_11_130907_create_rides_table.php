4793c1b446855be57e96e4682320ef9e67d6d9dd
Ozols <its27eo1@odin.local>
2017-08-14 10:20:52 +0300

    Edited welcome page...

---
+++ database/migrations/2017_08_11_130907_create_rides_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rides', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('userId');
+            $table->string('address');
+            $table->time('leavingTime');
+            $table->string('invoice');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rides');
+    }
+}
