bde76b8a47646f9e0e47862996820d49f196cd50
Ezols <Edvards.ozols@gmail.com>
2017-09-17 21:15:55 +0300

    Ēmail configured

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'taxi@transcom.lv'),
+        'name' => env('MAIL_FROM_NAME', 'Transcom Taxi'),
     ],
 
     /*
--- resources/views/showusers.blade.php
+++ resources/views/showusers.blade.php
@@ -1,14 +1,34 @@
-<style>
+@extends('layouts.app')
 
-</style>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Users</div>
+
+                <div class="panel-body">
+                     <table class="table">
+
+                        <tr>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
 
 
-
-@foreach($users as $user)
-    <ul>
-        <a href="{{ url('/updateuser', $user->id) }}"><li>{{ $user->name }}</li></a>
-        <a href="{{ url('/deleteuser', $user->id) }}">Delete</a>
-    </ul>
-@endforeach
-
-<a href="/home"><p>Back to home</p></a>
+                        @foreach($users as $user)
+                        <tr>
+                            <td><a href="{{ url('/updateuser', $user->id) }}">{{ $user->name }}</a></td>
+                            <td>{{ $user->role }}</td>
+                            <td><a href="{{ url('/deleteuser', $user->id) }}">Delete</a></td>
+                        </tr>
+                        @endforeach
+                
+                     </table>                     
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
